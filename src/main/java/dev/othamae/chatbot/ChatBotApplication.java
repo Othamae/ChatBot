51af9a5403520ac7b2dd64fe69657bbfc98f05af
Othamae <vero.gconesa@gmail.com>
2023-11-10 08:51:56 +0100

    feat: :truck: Changed name

--- src/main/java/dev/othamae/chatbot/ChatBotApplication.java
+++ src/main/java/dev/othamae/chatbot/ChatBotApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package dev.othamae.chatbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatBotApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatBotApplication.class, args);
+	
 	}
 
 }
