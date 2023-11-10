057a2d1905b900be8dca283eb45c4af89be3be79
Othamae <vero.gconesa@gmail.com>
2023-11-10 10:43:35 +0100

    feat: :sparkles: Added solr to app.properties

--- src/main/java/dev/othamae/chatbot/config/SolrConfig.java
+++ src/main/java/dev/othamae/chatbot/config/SolrConfig.java
@@ -1,13 +1,21 @@
 package dev.othamae.chatbot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SolrConfig {
 
-    public final static String SOLR_BASE_URL = "http://localhost:8983/solr/";
-    public final static String SOLR_CORE = "chatbot";
-    public final static String SOLR_CONTENT_FIELD = "content";
-    public final static String SOLR_CONTENT_KEY = "key";
+    @Value("${SOLR_BASE_URL}")
+    public static String SOLR_BASE_URL;
+
+    @Value("${SOLR_CORE}")
+    public static String SOLR_CORE;
+    
+    @Value("${SOLR_CONTENT_FIELD}")
+    public static String SOLR_CONTENT_FIELD;
+    
+    @Value("${SOLR_CONTENT_KEY}")
+    public static String SOLR_CONTENT_KEY;
 
 }
